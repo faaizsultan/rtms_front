bf94205fe2489ed4c85687c4772623ddb53f09b1
Faaiz Sultan <faaizsultan1@gmail.com>
2024-07-26 14:31:23 +0500

    Added Offline/Online Devices FInal

--- src/services/devices.js
+++ src/services/devices.js
@@ -71,3 +71,50 @@ export async function getTotalBranches(token) {
       return null;
     }
   }
+
+export async function getTotalFaultyDevices(token) {
+    const api_url = "https://nvback.newvative.com/manage/faultydevices/";
+    // const api_url = "http://localhost:8000/manage/offlinedevices/";
+    const result = "JWT ".concat(token.replace(/"/g, ' '));
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': result
+      },
+    };
+    // console.log("Request Options In Devices:", requestOptions);
+    try {
+      const response = await fetch(api_url, requestOptions);
+      const data = await response.json(); // Use await here
+      return data.faultydev;
+    } catch (error) {
+      // Handle any errors that occur during the fetch
+      console.error('Error:', error);
+      // return { success: false, devices: 0 };
+      return 0;
+    }
+  }
+
+  export async function getTotalOfflineDevices(token) {
+    const api_url = "https://nvback.newvative.com/manage/offlinedevices/";
+    const result = "JWT ".concat(token.replace(/"/g, ' '));
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': result
+      },
+    };
+    // console.log("Request Options In Devices:", requestOptions);
+    try {
+      const response = await fetch(api_url, requestOptions);
+      const data = await response.json(); // Use await here
+      return data.offlinedev;
+    } catch (error) {
+      // Handle any errors that occur during the fetch
+      console.error('Error:', error);
+      // return { success: false, devices: 0 };
+      return 0;
+    }
+  }
