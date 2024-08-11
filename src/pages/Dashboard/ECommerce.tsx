9d5e0cddebe73de5c0b259901d034949b45134a2
Faaiz Sultan <faaizsultan1@gmail.com>
2024-08-11 20:11:15 +0500

    Maps In Progress

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -53,7 +53,7 @@ const ECommerce: React.FC = (props) => {
         setOloading(false);
         
         
-        const b=await getDeviceData(token);
+        const b=await getDeviceData(token,'o3FDZo6axE');
         console.log(b);
 
 
@@ -179,7 +179,7 @@ const ECommerce: React.FC = (props) => {
 
       </div>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        {<LineGraph />}
+        {/* {<LineGraph />} */}
         {/* <LineChart /> */}
         {/* <ChartOne />
         <ChartTwo />
