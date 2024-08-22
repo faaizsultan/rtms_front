e58425536990cb9a3bc4b3f3251945d9c2453386
Faaiz Sultan <faaizsultan1@gmail.com>
2024-08-22 14:09:54 +0500

    Maps without 200 Equipmetn Temperature

--- src/components/Maps/GoogleMaps.tsx
+++ src/components/Maps/GoogleMaps.tsx
@@ -1,61 +1,53 @@
-import React, { useState } from 'react';
-import {APIProvider, Map } from '@vis.gl/react-google-maps';
-import  HoverableMarker  from './HoverableMarker';
+import React from 'react';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import HoverableMarker from './HoverableMarker';
 import Loader from '../../common/Loader';
 
 interface GoogleMapsProps {
-    api_key:  string;
-    data : Array<{
-    id: number,
-    time: string,
+  api_key: string;
+  data: Array<{
+    id: number;
+    time: string;
     sensorData: {
-        lat : number,
-        long: number,
-        equipment_temp :number,
-      };
-    spot_id : string,
-    }>;
-  }
+      lat: number;
+      long: number;
+      equipment_temp: number;
+    };
+    spot_id: string;
+  }>;
+}
 
+const GoogleMaps: React.FC<GoogleMapsProps> = ({ api_key, data }) => {
+  // Filter out records where equipment_temp is 200
+  const filteredData = data.filter((item) => item.sensorData.equipment_temp !== 200);
 
-const GoogleMaps : React.FC<GoogleMapsProps> = ({api_key ,data}) => {
-
-    return (
-        <>
-        
-       
-
-        { data ? (
-            <APIProvider apiKey={api_key}>
-                <Map
-                zoom={12} defaultCenter={{lat : data[0].sensorData.lat , lng: data[0].sensorData.long}}
-                style={{width: '100vw', height: '100vh'}}
-                
-                defaultZoom={3}
-                gestureHandling={'auto'}
-                disableDefaultUI={false}
-              >
-                  
-                  { 
-                      data.map( (item) => (
-                        <HoverableMarker  
-                          key={item.id}
-                          position={{lat: item.sensorData.lat, lng: item.sensorData.long}}
-                          equipment_temp={item.sensorData.equipment_temp}
-                          time={item.time}
-                        />
-                        ))
-                    }
-                </Map>
-              </APIProvider>
-              ) : (<Loader/>)
-          }
-            
-           
-     
-     
-        </>
-      );
-    }; 
+  return (
+    <>
+      {filteredData.length ? (
+        <APIProvider apiKey={api_key}>
+          <Map
+            zoom={12}
+            defaultCenter={{ lat: filteredData[0].sensorData.lat, lng: filteredData[0].sensorData.long }}
+            style={{ width: '100vw', height: '100vh' }}
+            defaultZoom={3}
+            gestureHandling={'auto'}
+            disableDefaultUI={false}
+          >
+            {filteredData.map((item) => (
+              <HoverableMarker
+                key={item.id}
+                position={{ lat: item.sensorData.lat, lng: item.sensorData.long }}
+                equipment_temp={item.sensorData.equipment_temp}
+                time={item.time}
+              />
+            ))}
+          </Map>
+        </APIProvider>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
+};
 
 export default GoogleMaps;
